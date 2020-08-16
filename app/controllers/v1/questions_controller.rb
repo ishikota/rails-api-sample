fdb97b5ad28c92fefe8ded19b668867d38504076
kota-ishimoto <kota-ishimoto@cookpad.com>
2020-08-16 12:10:41 +0900

    Add sample resource

---
+++ app/controllers/v1/questions_controller.rb
@@ -0,0 +1,39 @@
+class V1::QuestionsController < ApiController
+  def index
+    questions = Question.all
+    render json: questions
+  end
+
+  def show
+    question = Question.find(params[:id])
+    render json: question
+  end
+
+  def create
+    question = Question.new(question_params)
+    if question.save
+      render json: question
+    else
+      render_error_message(status: :unprocessable_entity, message: question.errors.messages)
+    end
+  end
+
+  def update
+    question = Question.find(params[:id])
+    if question.update(question_params)
+      render json: question
+    else
+      render_error_message(status: :unprocessable_entity, message: question.errors.messages)
+    end
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+  end
+
+  private
+
+  def question_params
+    params.permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
+    resources :questions
   end
   get '*path', :to => 'api#render_404'
 end
